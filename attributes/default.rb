b489c8fa954b82fea1aacfacbd4dcb3bc07121ee
Wang Zhijun <so@idcf.jp>
2016-06-17 17:16:33 +0900

    Erlangのバージョンを切り離す

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,5 @@
 default['erlang']['dev-packages'] = %w{wxGTK-devel unixODBC-devel}
 
-default['erlang']['file_name'] = "erlang-17.5"
-default['erlang']['url'] = "https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_17.5-1~centos~7_amd64.rpm"
+default['erlang']['version'] = "17.5"
+default['erlang']['file_name'] = "erlang-#{default['erlang']['version']}"
+default['erlang']['url'] = "https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_#{default['erlang']['version']}-1~centos~7_amd64.rpm"
