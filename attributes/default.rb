a96c50b65bca16d2bcc7e13823879f13656d9ee7
Wang Zhijun <so@idcf.jp>
2016-06-17 18:20:51 +0900

    MajorとMinorバージョン分ける

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,5 @@
 default['erlang']['dev-packages'] = %w{wxGTK-devel unixODBC-devel}
 
-default['erlang']['version'] = "17.5"
-default['erlang']['url'] = "https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_#{default['erlang']['version']}-1~centos~7_amd64.rpm"
+default['erlang']['major_version'] = "17"
+default['erlang']['minor_version'] = "5"
+default['erlang']['url'] = "https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_#{default['erlang']['major_version']}.#{default['erlang']['minor_version']}-1~centos~7_amd64.rpm"
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,7 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-file_name = "erlang-#{node['erlang']['version']}"
+file_name = "erlang-#{node['erlang']['major_version']}.#{node['erlang']['minor_version']}"
 case node['platform_family']
 when 'rhel'
   remote_file "#{Chef::Config['file_cache_path']}/#{file_name}.rpm" do
@@ -17,7 +17,7 @@ when 'rhel'
 
   package 'erlang-erts' do
     action :purge
-    not_if "erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().'  -noshell | grep 17"
+    not_if "erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().'  -noshell | grep #{node['erlang']['major_version']}"
   end
 
   # wx関係のパッケージが必要だからインストール
