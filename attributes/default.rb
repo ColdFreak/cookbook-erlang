8d07217d21a7210e693487ab0baebd9a0e312bf4
Wang Zhijun <so@idcf.jp>
2016-06-17 18:13:20 +0900

    Fix erlang cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 default['erlang']['dev-packages'] = %w{wxGTK-devel unixODBC-devel}
 
-default['erlang']['url'] = "https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_#{node['erlang']['version']}-1~centos~7_amd64.rpm"
+default['erlang']['version'] = "17.5"
+default['erlang']['url'] = "https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_#{default['erlang']['version']}-1~centos~7_amd64.rpm"
