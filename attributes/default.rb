5ed4a8bf9a872f1b1f19a148f429925fb8e54aa6
Wang Zhijun <so@idcf.jp>
2016-07-20 08:34:59 +0900

    デフォルトErlang 18.3をインストールするように修正

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['erlang']['dev-packages'] = %w{wxGTK-devel unixODBC-devel}
 
-default['erlang']['major_version'] = "17"
-default['erlang']['minor_version'] = "5"
+default['erlang']['major_version'] = "18"
+default['erlang']['minor_version'] = "3"
 default['erlang']['url'] = "https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_#{default['erlang']['major_version']}.#{default['erlang']['minor_version']}-1~centos~7_amd64.rpm"
